791cb9ca1aab30346ba98aa68f5ad5124aa27079
Karen Pion <karenlpion@gmail.com>
2021-08-29 12:54:10 -0400

    starting to seed data

--- seed/seeds.js
+++ seed/seeds.js
@@ -1,3 +1,19 @@
-// seed data
-// const db = rewuire('../config/connection');
-// const lotrJourneySeeds = require('./lotrJourney');
+// // seed data
+// const db = require('../config/connection');
+// const { 
+//     LotrJourney, 
+//     Trail, 
+// } = require('../models');
+
+// const journeyData = require('./lotrJourney');
+// const trailData = require('./trailSeeds');
+
+// db.once('open', async () => {
+//     await LotrJourney.deleteMany({});
+//     await Trail.deleteMany({});
+
+//    await LotrJourney.insertMany(journeyData);
+//     const trails = 
+//     console.log("Data Seeded!");
+//     process.exit(0);
+// });
