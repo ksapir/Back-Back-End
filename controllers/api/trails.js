103682d66aeebbce67462d01697cc8f29317ef4f
Karen Pion <karenlpion@gmail.com>
2021-09-02 01:38:19 -0400

    some light edits

--- controllers/api/trails.js
+++ controllers/api/trails.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-require ('dotenv').config()
 const Trail = require('../../models/Trail')
 
 // Gets all trails
@@ -13,35 +12,6 @@ router.get("/", (req,res) =>{
     }
 })
 })
-
-// router.get("/:location", (req,res) =>{
-//   const location = req.params.location
-//   Trail.findById(location), (err, data) => {
-//    if (err) {
-//      console.log(err)
-//      return res.send(err)
-//    } else {
-//      return res.json(data)
-//    }
-// }})
-
-//user has favorite trails
-
-//see all fave trails
-
-// router.post('/profile/:id/:journey/currentwalk', (req,res) => {
-//     let userId = req.params.id
-//     let journeyId = req.params.journey
-//     let userMilesWalked = req.body.userMilesWalked
-//     db.Journey.findById(journeyId)
-//     .then(dbJourney => {
-//         db.Journey.update({id:req.params.id}), {$set: {distance: userMilesWalked + dbJourney.totalMiles}}
-//         res.json(dbJourney);
-//       })
-//       .catch(err => {
-//         res.json(err);
-//       });    
-// })
 
  module.exports = router
   
--- seed/seeds.js
+++ seed/seeds.js
@@ -1,15 +1,15 @@
 const db = require('../config/db');
-const  LotrJourney  = require('../models/LotrJourney');
+const  Journey  = require('../models/Journey');
 const  Trail  = require('../models/Trail');
 
 const journeyData = require('./lotrJourney.js');
 const trailData = require('./trailSeeds.js');
 
 db.once('open', async () => {
-  await LotrJourney.deleteMany({});
+  await Journey.deleteMany({});
   await Trail.deleteMany({});
 
-  await LotrJourney.insertMany(journeyData);
+  await Journey.insertMany(journeyData);
   await Trail.insertMany(trailData)
 
   console.log('Data Seeded!');
